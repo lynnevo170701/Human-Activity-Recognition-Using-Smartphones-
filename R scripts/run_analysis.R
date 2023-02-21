8cd9dc9ad4a425c0d3d0ef4dcdd07e8aa7964d04
lynnevo170701 <108290845+lynnevo170701@users.noreply.github.com>
2023-02-21 13:18:49 +0100

    run_analysis file with script analysis and tidy_data file

---
+++ R scripts/run_analysis.R
@@ -0,0 +1,54 @@
+#download zip files and unzip files
+library(tidyverse)
+file_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(file_url, destfile = "./mydata.zip")
+unzip("./mydata.zip")
+
+#read activity labels + features
+#using read.delim() to read .txt files
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt"
+                             , header = F
+                             , col.names = c("ClassLabels", "ActivityName"))
+features <- read.table("./UCI HAR Dataset/features.txt"
+                       , header = F
+                       , col.names = c("Number", "FeatureName"))
+mean_std_indices <- grep("(mean|std)\\(\\)"
+                            , features$FeatureName) #two backslash to specify parenthesis ()
+measurements <- features[mean_std_indices, "FeatureName"]
+measurements <- gsub('[()]', "", measurements)
+
+#read train data sets
+train_df <- read.table("./UCI HAR Dataset/train/X_train.txt"
+                      
+                       , header = F)
+train_df <- train_df[, mean_std_indices] #only select the mean and std columns
+colnames(train_df) = measurements
+train_labels <- read.table("./UCI HAR Dataset/train/y_train.txt"
+                           , header = F
+                           , col.names = "Activity")
+train_subject <- read.table("./UCI HAR Dataset/train/subject_train.txt"
+                           , header = F
+                           , col.names = "SubjectNumber")
+train_df <- cbind(train_subject, train_labels, train_df)
+
+#read test data sets
+test_df <- read.table("./UCI HAR Dataset/test/X_test.txt"
+                      , header = F)
+test_df <- test_df[, mean_std_indices]
+colnames(test_df) = measurements
+test_labels <- read.table("./UCI HAR Dataset/test/y_test.txt"
+                          , header = F
+                          , col.names = "Activity")
+test_subject <- read.table("./UCI HAR Dataset/test/subject_test.txt"
+                           , header = F
+                           , col.names = "SubjectNumber")
+test_df <- cbind(test_subject,test_labels, test_df)
+
+#merge two data sets
+merged_df <- rbind(train_df,test_df)
+
+#changing acitivity labels to corresponding activity names
+merged_df$Activity <- sapply(merged_df$Activity, function(x) {activity_labels$ActivityName[x]})
+
+#export merged_df as tidy_data.txt file
+write.table(merged_df, file = "tidy_data.txt", quote = TRUE)
